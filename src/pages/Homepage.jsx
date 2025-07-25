3ee97d4950d6aa2557c454136d52a35e4b0e33bd
iyappan <ii129074ij@gmail.com>
2025-07-25 10:04:45 +0530

    Footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Projects from "../components/Project";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Cerification from "../components/Cerification";
 import "../index.css";
 
